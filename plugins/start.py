2f2720a1f43684b41be04cb1be74d1fb55f388e9
Asifasif6625 <86577020+Asifasif6625@users.noreply.github.com>
2021-11-21 12:35:44 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -58,7 +58,7 @@ async def start_command(client: Client, message: Message):
                 ids = [int(int(argument[1]) / abs(client.db_channel.id))]
             except:
                 return
-        temp_msg = await message.reply("Please wait...")
+        temp_msg = await message.reply("ആശാനെ ഒന്ന് കാത്തുനിൽക്കണേ ഫയല്‍ ഇപ്പൊ തരാം... /nദേ... പോയി ദാ... വന്നു.. ")
         try:
             messages = await get_messages(client, ids)
         except:
